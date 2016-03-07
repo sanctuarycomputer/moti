fd9011941d032632717189f4dd52442e21f34b7f
Sebastian Odell <sebastianodell@gmail.com>
2016-03-07 11:17:29 -0500

    added hello world component

--- app/app.js
+++ app/app.js
@@ -1,1 +1,14 @@
-console.log('Hello!')
+var React    = require('react'),
+    ReactDOM = require('react-dom');
+
+var HelloWorld = React.createClass({
+  render: function(){
+    return(
+      <h1>
+        Hello World
+      </h1>
+    )
+  }
+});
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
