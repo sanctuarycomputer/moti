65d78c7991f9e632642169da526f72e2b31f9a4a
Daniel Hunninghake <daniel@drip.fm>
2016-05-06 16:29:21 -0400

    request photos from IG and update state with them

--- app/lib/initialize.js
+++ app/lib/initialize.js
@@ -1,8 +1,15 @@
 import { oAuthBegin } from '../actions/oauth';
+import { fetchPhotosForHashtag } from '../actions/gallery';
+import { getHashtagPhotos } from '../api/instagram';
 
 export default function(dispatch) {
   const accessToken = window.localStorage.getItem('instagramAccessToken');
+
   if (accessToken) {
-    oAuthBegin('instgram', accessToken)(dispatch);
+    oAuthBegin('instgram', accessToken)(dispatch).then(currentUser => {
+      return fetchPhotosForHashtag('bortsimpson', accessToken)(dispatch).then(() => {
+        return fetchPhotosForHashtag('houndstooth', accessToken)(dispatch);
+      })
+    });
   }
 }
