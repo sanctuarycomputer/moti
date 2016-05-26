35654dd0b7d41acdc1db0aa0bf170a651d079125
Hugh Francis <me@hughfrancis.me>
2016-05-26 12:13:41 -0400

    Fix~!

--- app/config/Environment.js
+++ app/config/Environment.js
@@ -3,14 +3,14 @@ let ENV = {};
 if (!process.env.NODE_ENV) {
   ENV = {
     INSTAGRAM_CLIENT_ID: '965fc4549fa04e5f9a4615435bb9ecdc',
-    OAUTH_REDIRECT_URI: 'museumoftheinternet.dev:8080',
+    OAUTH_REDIRECT_URI: 'http://museumoftheinternet.dev:8080/',
     INSTAGRAM_API_ENDPOINT: 'https://api.instagram.com',
     FIREBASE_ROOT_URL: 'incandescent-fire-671.firebaseIO.com'
   }
 } else {
   ENV = {
     INSTAGRAM_CLIENT_ID: '965fc4549fa04e5f9a4615435bb9ecdc',
-    OAUTH_REDIRECT_URI: 'https://incandescent-fire-671.firebaseapp.com',
+    OAUTH_REDIRECT_URI: 'https://incandescent-fire-671.firebaseapp.com/',
     INSTAGRAM_API_ENDPOINT: 'https://api.instagram.com',
     FIREBASE_ROOT_URL: 'incandescent-fire-671.firebaseIO.com'
   }
