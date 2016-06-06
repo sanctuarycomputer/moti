1dcdb96da62527ed73724e0d3759047fffbc2a5a
Sebastian Odell <sebastianodell@gmail.com>
2016-06-06 12:46:29 -0400

    removed flashMessage file

--- app/lib/flashMessage.js
+++
@@ -1,5 +0,0 @@
-export default {
-  saved:'Saved to the permanent collection',
-  beenSaved: 'You\'ve already saved this to the permanent collection',
-  cantAuth: 'An error occurred, please try again.'
-};
